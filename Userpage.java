1b9d915ff18f8661f7c96a3daf971b0902b3821d
Shashanksingh360 <shashank.shashank.singh360@gmail.com>
2017-06-30 13:00:33 +0530

    Create Userpage.java

---
+++ Userpage.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+class Userpage {
+
+	void user() {
+		
+		JFrame userFrame=new JFrame("UserFrame");
+		
+		userFrame.setBounds(0,0,500,500);
+		userFrame.setVisible(true);
+		
+
+	}
+
+}
